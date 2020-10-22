c50eaac27ca497eb04ef5ec29316feeb1e9cad57
denmercs <denmercs@gmail.com>
2020-10-22 04:00:39 -0500

    removed authentication and refactor routes

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
-const authRouter = require("./routes/authRouter");
 const facebookRouter = require("./routes/facebookRouter");
 
 const server = express();
@@ -11,12 +10,11 @@ server.use(cors());
 server.use(helmet());
 server.use(express.json());
 
-server.use("/auth", authRouter);
 server.use("/facebook", facebookRouter);
 
 server.get("/", (req, res) => {
   res.json({
-    api: "Denmercs Photography BE is running"
+    api: "Denmercs Photography BE is running",
   });
 });
 
